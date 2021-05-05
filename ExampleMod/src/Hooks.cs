752bcdf84be15d7961b156da4fef9cef89e3f5a9
Frityet <ambhogal01@gmail.com>
2021-05-04 21:14:56 -0700

    Initial commit

---
+++ ExampleMod/src/Hooks.cs
@@ -0,0 +1,26 @@
+﻿namespace ExampleMod
+{
+	public class Hooks
+	{
+
+		public Hooks()
+		{
+			Hook();
+		}
+
+		public void Dispose()
+		{
+			Unhook();
+		}
+
+		private void Hook()
+		{
+
+		}
+
+		private void Unhook()
+		{
+
+		}
+	}
+}
---
+++ ExampleMod/src/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Deli.Setup;
+
+namespace ExampleMod
+{
+	public class Plugin : DeliBehaviour
+	{
+		private readonly Hooks _hooks;
+
+		public Plugin()
+		{
+			_hooks = new Hooks();
+		}
+
+		private void OnDestroy()
+		{
+			_hooks?.Dispose();
+		}
+	}
+}
